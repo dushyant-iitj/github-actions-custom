4b05a54474109332699cbef759cc595311813a8d
Nest <nsitseo@gmail.com>
2025-09-03 21:53:17 +0530

    initial version

---
+++ index.js
@@ -0,0 +1,10 @@
+import * as core from "@actions/core";
+import * as core from "@actions/github";
+
+try {
+  const name = core.getInput("name");
+  console.log(`Hello ${name} from Custom`);
+  core.setOutput("greeting", output_value);
+} catch (error) {
+  core.setFailed(error.message);
+}
